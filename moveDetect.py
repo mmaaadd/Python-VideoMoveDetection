e51df26c03d07ad510378d69980bec3212761f95
Krzysztof W <krzwasilewski@outlook.com>
2022-04-12 19:04:55 +0200

    Addition of motion time recording to CSV

--- moveDetect.py
+++ moveDetect.py
@@ -1,13 +1,18 @@
-import cv2
+from datetime import datetime
+import cv2, pandas, time
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 #first frame set up
 first_frame = None
 check, first_frame = video.read ()
 first_frame = cv2.cvtColor(first_frame,cv2.COLOR_BGR2GRAY)
 first_frame = cv2.GaussianBlur(first_frame, (21,21),0)
-# cv2.imshow("First_frame",first_frame)
+status = 0 #variable to store motion det. status
+single_event = []
+events = []
+df = pandas.DataFrame(columns=["Start","End"])
+
 
 while True:
     check, frame = video.read ()
@@ -19,10 +24,21 @@ while True:
 
     (cnts,__) = cv2.findContours(tresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    number_of_contours = 0
     for contour in cnts:
         if cv2.contourArea(contour) > 100:
             (x,y,w,h) = cv2.boundingRect(contour)
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            number_of_contours = number_of_contours + 1
+    if number_of_contours > 0:
+        if status == 0:
+            single_event = [datetime.now(), None]
+        status = 1
+    else:
+        if status == 1:
+            single_event[1] = datetime.now()
+            events.append (single_event)
+        status = 0
 
     cv2.namedWindow("Tytul");
     cv2.moveWindow("Tytul", 50,50);    
@@ -32,7 +48,13 @@ while True:
 
     key = cv2.waitKey(1)
     if key == ord ('q'):
+        if single_event[0] != None and single_event[1] == None:
+            single_event[1] = datetime.now()
+            events.append (single_event)           
         break
-
+print(events)
 video.release()
 cv2.destroyAllWindows()
+
+df = pandas.DataFrame(events, columns = ['Start','End'])
+df.to_csv("motion.csv")
